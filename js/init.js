d99da4d9544e83d6a06b5663ad1c8be2c074ee0f
Bryan Benson <bryanbenson83@gmail.com>
2016-04-15 20:38:56 -0400

    move countdown to init.js

--- js/init.js
+++ js/init.js
@@ -1,20 +1,45 @@
-//Hook up the tweet display
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+
 
 $(document).ready(function() {
 						   
-	$("#countdown").countdown({
-				date: "1 december 2015 12:00:00",
-				format: "on"
-			},
-			
-			function() {
-				// callback function
-			});
-
-    $(".tweet").tweet({
-        username: "flashuser",
-        count: 3,
-        loading_text: "loading tweets..."
-    });
-
+  var deadline = new Date(Date.parse(new Date("27 april 2016 17:00:00"));
+  initializeClock('clockdiv', deadline);
 });	
