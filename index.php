8cd3034bacee5f210a357b77886e2a0098066b79
Bryan Benson <bryanbenson83@gmail.com>
2016-04-25 15:44:42 -0400

    Add email subscription

--- index.php
+++ index.php
@@ -8,23 +8,16 @@ if(isset($_POST['submit'])){
    $message = new SendGrid\Email();
    $message->addTo('bryanbenson83@gmail.com')->
               setFrom($_POST['email'])->
-                       setSubject("Study Clash Email List submission")->
-                                setText("The sender of this email wishes to be informed about study clash.")->
-                                         setHtml('<strong>The sender of this email wishes to be informed about study clash.</strong>');
-   $response = $sendgrid->send($message); 
-   $message = new SendGrid\Email();
-   $message->addTo('scott.benson96@gmail.com')->
-              setFrom($_POST['email'])->
-                       setSubject("Study Clash Email List submission")->
-                                setText("The sender of this email wishes to be informed about study clash.")->
-                                         setHtml('<strong>The sender of this email wishes to be informed about study clash.</strong>');
+                       setSubject("Lunarshucks Email List submission")->
+                                setText("The sender of this email wishes to be informed about Lunar Shucks.")->
+                                         setHtml('<strong>The sender of this email wishes to be informed about Lunar Shucks.</strong>');
    $response = $sendgrid->send($message); 
    $message = new SendGrid\Email();
    $message->addTo($_POST['email'])->
               setFrom('studyclash@gmail.com')->
                        setSubject("Study Clash Email List Submission")->
-                                setText("Hi " .  $_POST['email'] . "!" . "Thanks for your interest in Study Clash! We are working hard to get our site up and running and will be sure to keep you updated. -- The Study Clash Team")->
-                                         setHtml("<p>Hi " .  $_POST['email'] . "! </p><br></br>" . "<p>Thanks for your interest in Study Clash! We are working hard to get our site up and running and will be sure to keep you updated. </p><br></br> </p>-- The Study Clash Team</p>");
+                                setText("Hi " .  $_POST['email'] . "!" . "Thanks for your interest in Lunar Shucks! We need some help defeating the Archeologist, so stay tuned for more info!")->
+                                         setHtml("<p>Hi " .  $_POST['email'] . "! </p><br></br>" . "<p>Thanks for your interest in Lunar Shucks! We need some help defeating the Archeologist, so stay tuned for more info!</p><br></br> </p>-- The Study Clash Team</p>");
    $response = $sendgrid->send($message); 
    echo "Mail Sent. Thank you.";
 }
